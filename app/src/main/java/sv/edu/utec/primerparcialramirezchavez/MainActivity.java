c4f66f91a2262354151e9022249d60c78a0b4062
Josue Eliezer Ramirez Chavez <2521132019@mail.utec.edu.sv>
2023-02-22 21:43:08 -0600

    SUBIR1

--- app/src/main/java/sv/edu/utec/primerparcialramirezchavez/MainActivity.java
+++ app/src/main/java/sv/edu/utec/primerparcialramirezchavez/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
     EditText edtusuario,etcontraseña;
 
-    TextView txtresp;
+    TextView txtrespi;
     Button btningresar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +21,19 @@ public class MainActivity extends AppCompatActivity {
 
         edtusuario=findViewById(R.id.edtuser);
         etcontraseña=findViewById(R.id.edtcontra);
-        txtresp=findViewById(R.id.txtresp);
+        txtrespi=findViewById(R.id.txtresp);
 
-        public void ingre (View v){
+        public void ingre (View View){
             String Usuario, contraseña, resp;
 
-            Usuario = etUsuario.getText().ToString();
+            Usuario = edtusuario.getText().toString();
             contraseña=etcontraseña.getText().toString();
 
             if(Usuario.equals("parcialETps1")&&(contraseña.equals("p4rC14l#tp$"))){
-                txtresp.setText("conrrecto");
-
+                txtrespi.setText("conrrecto");
         }
             else{
-                txtresp.setText("incorrecto");
+                txtrespi.setText("incorrecto");
             }
         }
     }
